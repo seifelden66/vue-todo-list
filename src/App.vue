35afcdca3e8d3818f6424d533d168c80d085e99e
seifelden66 <seifelden66@gmail.com>
2023-01-31 23:02:48 +0200

    second commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <div class="the-inputs">
+      <h2>enter task</h2>
+      <textarea v-model="task" />
+    </div>
+    <div class="tasks">
+      <ul>
+        <li v-for="(task, i) in theList" :key="i">
+          <div class="tas">{{ task }}</div>
+          <button @click="removeTask(i)" class="warn">x</button>
+        </li>
+      </ul>
+    </div>
+    <div class="buttons">
+      <button @click="addTask" :disabled="!task">add task</button>
+      <button @click="clearTasks" class="warn">clear list</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      theList: [],
+      task: undefined,
+    };
+  },
+  methods: {
+    addTask() {
+      this.theList.push(this.task);
+      this.task = undefined;
+    },
+    removeTask(i) {
+      this.theList.splice(i, 1);
+    },
+    clearTasks() {
+      this.theList = [];
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.349);
+  padding: 20px;
+  width: 50%;
+  margin: auto;
+  transform: translateY(150px);
+
+  .buttons {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .the-inputs {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    align-items: center;
+  }
+  textarea {
+    border-radius: 10px;
+    border: 1px solid rgba(0, 0, 0, 0.349);
+    padding: 10px;
+    height: 30px;
+  }
+
+  .tasks {
+    margin-top: 20px;
+    border: 1px solid rgba(12, 10, 10, 0.24);
+    border-radius: 10px;
+    min-height: 200px;
+    align-items: center;
+    li {
+      display: grid;
+      grid-template-columns: 3fr 0.5fr;
+      margin-top: 0.5rem;
+      margin-right: 1rem;
+      align-items: center;
+      .tas {
+        border: 1px solid rgba(12, 10, 10, 0.178);
+        border-radius: 10px;
+        padding: 0.5rem;
+        width: 80%;
+      }
+    }
+  }
+}
+</style>
